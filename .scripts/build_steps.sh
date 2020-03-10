b09c8bdd249e7801b902c79019cba7fd4b01419a
Duncan Macleod <duncan.macleod@ligo.org>
2020-03-10 11:01:40 +0000

    use executable path for yum man package
    since centos 6 and centos 8 have different package names

--- .scripts/build_steps.sh
+++ .scripts/build_steps.sh
@@ -31,7 +31,7 @@ source run_conda_forge_build_setup
 # "recipe/yum_requirements.txt" file. After updating that file,
 # run "conda smithy rerender" and this line will be updated
 # automatically.
-/usr/bin/sudo -n yum install -y man-db
+/usr/bin/sudo -n yum install -y /usr/bin/man
 
 
 # make the build number clobber
